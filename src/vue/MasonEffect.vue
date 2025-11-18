82b689c5cdc15b09965a760806ae41b73aad78ad
fe.hyunsu <fe.hyunsu@gmail.com>
2025-11-18 10:02:29 +0900

    fix: Convert Vue component to TypeScript and fix import paths

--- src/vue/MasonEffect.vue
+++ src/vue/MasonEffect.vue
@@ -2,91 +2,93 @@
   <div ref="container" :class="className" :style="style"></div>
 </template>
 
-<script>
-import { defineComponent, ref, onMounted, onBeforeUnmount, watch } from 'vue';
-import { MasonEffect } from '../core/index.js';
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount, watch } from 'vue';
+import { MasonEffect, type MasonEffectOptions } from '../core/index';
 
-export default defineComponent({
-  name: 'MasonEffect',
-  props: {
-    text: {
-      type: String,
-      default: 'mason effect',
-    },
-    densityStep: {
-      type: Number,
-      default: 2,
-    },
-    maxParticles: {
-      type: Number,
-      default: 3200,
-    },
-    pointSize: {
-      type: Number,
-      default: 0.5,
-    },
-    ease: {
-      type: Number,
-      default: 0.05,
-    },
-    repelRadius: {
-      type: Number,
-      default: 150,
-    },
-    repelStrength: {
-      type: Number,
-      default: 1,
-    },
-    particleColor: {
-      type: String,
-      default: '#fff',
-    },
-    fontFamily: {
-      type: String,
-      default: 'Inter, system-ui, Arial',
-    },
-    fontSize: {
-      type: Number,
-      default: null,
-    },
-    width: {
-      type: Number,
-      default: null,
-    },
-    height: {
-      type: Number,
-      default: null,
-    },
-    devicePixelRatio: {
-      type: Number,
-      default: null,
-    },
-    className: {
-      type: String,
-      default: '',
-    },
-    style: {
-      type: Object,
-      default: () => ({}),
-    },
-    onReady: {
-      type: Function,
-      default: null,
-    },
-    onUpdate: {
-      type: Function,
-      default: null,
-    },
-  },
-  emits: ['ready', 'update'],
-  setup(props, { expose, emit }) {
-    const container = ref(null);
-    let instance = null;
+interface Props extends Partial<MasonEffectOptions> {
+  className?: string;
+  style?: Record<string, any>;
+}
 
-    const init = () => {
-      if (!container.value) return;
+const props = withDefaults(defineProps<Props>(), {
+  text: 'mason effect',
+  densityStep: 2,
+  maxParticles: 3200,
+  pointSize: 0.5,
+  ease: 0.05,
+  repelRadius: 150,
+  repelStrength: 1,
+  particleColor: '#fff',
+  fontFamily: 'Inter, system-ui, Arial',
+  fontSize: null,
+  width: null,
+  height: null,
+  devicePixelRatio: null,
+  className: '',
+  style: () => ({}),
+  onReady: null,
+  onUpdate: null,
+});
 
-      const options = {
+const emit = defineEmits<{
+  ready: [instance: MasonEffect];
+  update: [instance: MasonEffect];
+}>();
+
+const container = ref<HTMLElement | null>(null);
+let instance: MasonEffect | null = null;
+
+const init = () => {
+  if (!container.value) return;
+
+  const options: MasonEffectOptions = {
+    text: props.text,
+    densityStep: props.densityStep,
+    maxParticles: props.maxParticles,
+    pointSize: props.pointSize,
+    ease: props.ease,
+    repelRadius: props.repelRadius,
+    repelStrength: props.repelStrength,
+    particleColor: props.particleColor,
+    fontFamily: props.fontFamily,
+    fontSize: props.fontSize,
+    width: props.width,
+    height: props.height,
+    devicePixelRatio: props.devicePixelRatio,
+    onReady: (inst) => {
+      if (props.onReady) props.onReady(inst);
+      emit('ready', inst);
+    },
+    onUpdate: (inst) => {
+      if (props.onUpdate) props.onUpdate(inst);
+      emit('update', inst);
+    },
+  };
+
+  instance = new MasonEffect(container.value, options);
+};
+
+// props 변경 감지
+watch(
+  () => [
+    props.text,
+    props.densityStep,
+    props.maxParticles,
+    props.pointSize,
+    props.ease,
+    props.repelRadius,
+    props.repelStrength,
+    props.particleColor,
+    props.fontFamily,
+    props.fontSize,
+    props.width,
+    props.height,
+    props.devicePixelRatio,
+  ],
+  () => {
+    if (instance) {
+      instance.updateConfig({
         text: props.text,
         densityStep: props.densityStep,
         maxParticles: props.maxParticles,
@@ -100,91 +102,38 @@ export default defineComponent({
         width: props.width,
         height: props.height,
         devicePixelRatio: props.devicePixelRatio,
-        onReady: (inst) => {
-          if (props.onReady) props.onReady(inst);
-          emit('ready', inst);
-        },
-        onUpdate: (inst) => {
-          if (props.onUpdate) props.onUpdate(inst);
-          emit('update', inst);
-        },
-      };
+      });
+    }
+  }
+);
 
-      instance = new MasonEffect(container.value, options);
-    };
+onMounted(() => {
+  init();
+});
 
-    // props 변경 감지
-    watch(
-      () => [
-        props.text,
-        props.densityStep,
-        props.maxParticles,
-        props.pointSize,
-        props.ease,
-        props.repelRadius,
-        props.repelStrength,
-        props.particleColor,
-        props.fontFamily,
-        props.fontSize,
-        props.width,
-        props.height,
-        props.devicePixelRatio,
-      ],
-      () => {
-        if (instance) {
-          instance.updateConfig({
-            text: props.text,
-            densityStep: props.densityStep,
-            maxParticles: props.maxParticles,
-            pointSize: props.pointSize,
-            ease: props.ease,
-            repelRadius: props.repelRadius,
-            repelStrength: props.repelStrength,
-            particleColor: props.particleColor,
-            fontFamily: props.fontFamily,
-            fontSize: props.fontSize,
-            width: props.width,
-            height: props.height,
-            devicePixelRatio: props.devicePixelRatio,
-          });
-        }
-      }
-    );
+onBeforeUnmount(() => {
+  if (instance) {
+    instance.destroy();
+    instance = null;
+  }
+});
 
-    onMounted(() => {
-      init();
-    });
-
-    onBeforeUnmount(() => {
-      if (instance) {
-        instance.destroy();
-        instance = null;
-      }
-    });
-
-    // 외부에서 사용할 수 있는 메서드 노출
-    expose({
-      morph: (text) => {
-        if (instance) instance.morph(text);
-      },
-      scatter: () => {
-        if (instance) instance.scatter();
-      },
-      updateConfig: (config) => {
-        if (instance) instance.updateConfig(config);
-      },
-      destroy: () => {
-        if (instance) {
-          instance.destroy();
-          instance = null;
-        }
-      },
-    });
-
-    return {
-      container,
-    };
+// 외부에서 사용할 수 있는 메서드 노출
+defineExpose({
+  morph: (textOrOptions?: string | Partial<MasonEffectOptions>) => {
+    if (instance) instance.morph(textOrOptions);
+  },
+  scatter: () => {
+    if (instance) instance.scatter();
+  },
+  updateConfig: (config: Partial<MasonEffectOptions>) => {
+    if (instance) instance.updateConfig(config);
+  },
+  destroy: () => {
+    if (instance) {
+      instance.destroy();
+      instance = null;
+    }
   },
 });
 </script>
-
